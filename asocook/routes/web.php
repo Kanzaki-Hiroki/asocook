8568aeb6513c58c1d6db1e022fee195d07d394e2
Kanzaki-Hiroki <2301393@s.asojuku.ac.jp>
2024-10-25 09:48:57 +0900

    laravelを削除

--- asocook/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-#use App\Http\Controllers\;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get/post('ファイル名', [コントローラー名::class, 'メソッド名'])->name('route{{}}で呼び出す名前');
