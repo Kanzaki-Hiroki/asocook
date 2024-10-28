d1274e524fe4b84284e3e9c97c90e6cd04442670
Kanzaki-Hiroki <2301393@s.asojuku.ac.jp>
2024-10-28 09:59:13 +0900

    bootstrapファイル追加

---
+++ top.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div id="">
+            <p class="logo">
+                <img src="/img/" alt="">
+            </p>
+        </div>
+    </header>
+    <form action="">
+        <input type="text">
+        <input type="image" src="/img/" placeholder="キーワード検索">
+    </form>
+    <!-- <main></main> -->
+    <footer></footer>
+</body>
+</html>
