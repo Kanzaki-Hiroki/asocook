6a76e1f758077519b2512c57d35724cbdb8e2792
LINHSUANTU <2301383@s.asojuku.ac.jp>
2024-11-01 12:38:22 +0900

    支払い完了php作成

---
+++ shiharaikanryo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>支払い完了</title>
+</head>
+<body>
+    <div class="logo">
+        <img src="img/logo.png" alt="システムロゴ">
+    </div>
+    
+</body>
+</html>
