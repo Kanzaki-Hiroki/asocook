bc436c9122a639a18288710de58a2928c9876f12
LINHSUANTU <2301383@s.asojuku.ac.jp>
2024-11-06 11:17:19 +0900

    gm作成

---
+++ mypage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>管理者画面</title>
+</head>
+<body>
+    <div class="logo">
+        <img src="img/logo.png" alt="システムロゴ">
+    </div>
+    <p>マイページ</p>
+    
+</body>
+</html>
